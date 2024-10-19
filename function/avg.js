3ea5a1525e4527008c6a05e904322ee815809334
Pratham Raghuvanshi <141766108+Pratham22R@users.noreply.github.com>
2024-10-19 13:52:52 +0530

    Delete function directory

--- function/avg.js
+++
@@ -1,7 +0,0 @@
-function avgOfThreeNumbers(num1,num2,num3){
-    let sum = num1+num2+num3;
-    let avg = sum/3;
-    console.log(avg);
-}
-
-avgOfThreeNumbers(100,250,340);
--- function/dice.js
+++
@@ -1,6 +0,0 @@
-function dice(){
-    let dice = Math.floor(Math.random()*6)+1;
-    console.log(dice)
-}
-
-dice(); 
